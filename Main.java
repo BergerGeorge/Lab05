63c9742ee3ec02542bec87a15c89b6d1b246e90b
BergerGeorge <124177109+BergerGeorge@users.noreply.github.com>
2023-04-26 17:36:17 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args) {
+class BirthMonth {
+    public static void main(String[] args){
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Please enter the cost of your item:");
-        Double itemPrice = myObj.nextDouble();
-        ;
-        if (itemPrice <= 99)
-        {
-            double shippingCost = (0.02 * itemPrice);
-            double totalCost = (shippingCost + itemPrice);
-            System.out.println("Your shipping cost is " + shippingCost + " and your total cost is "+totalCost);
+        System.out.println("Please enter the number of your birth month:");
+        Double birthMonth = myObj.nextDouble();
+        if (birthMonth >= 13){
+            System.out.println("You entered an incorrect month value: "+birthMonth);
         }
-        else
-        {
-            System.out.println("Your shipping cost is free, and your total cost is "+ itemPrice);
+        else {
+            System.out.println("Your birth month is: "+birthMonth);
         }
     }
+}
+
 }
