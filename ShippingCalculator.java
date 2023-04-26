2d59e9f32ab7959b2f690c8a1b47d2a393e9c4e0
BergerGeorge <124177109+BergerGeorge@users.noreply.github.com>
2023-04-26 17:58:54 -0400

    Add files via upload

---
+++ ShippingCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ShippingCalculator {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Please enter the cost of your item:");
+        Double itemPrice = myObj.nextDouble();
+        ;
+        if (itemPrice <= 99)
+        {
+            double shippingCost = (0.02 * itemPrice);
+            double totalCost = (shippingCost + itemPrice);
+            System.out.println("Your shipping cost is " + shippingCost + " and your total cost is "+totalCost);
+        }
+        else
+        {
+            System.out.println("Your shipping cost is free, and your total cost is "+ itemPrice);
+        }
+    }
+}
